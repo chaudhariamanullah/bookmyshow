20eec54c0e52b43afdd90195fcc239ca7da946e7
amanullah <chaudhari.amanullah0@gmail.com>
2025-12-24 22:13:37 +0530

    Added Database Tables

--- backend/movie-service/src/server.ts
+++ backend/movie-service/src/server.ts
@@ -1,10 +1,4 @@
 import app from './app.js';
-import { getAllMovies } from './services/service.js';
-import type { Request, Response } from 'express';
-
-// app.get("/",(req:Request,res:Response)=>{
-//     res.json(getAllMovies());
-// });
 
 const PORT = 3000
 app.listen(PORT, () => {

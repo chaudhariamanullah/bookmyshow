18505f406d68bbded255ab4da297b9a780e6cc22
amanullah <chaudhari.amanullah0@gmail.com>
2026-01-21 19:30:00 +0530

    Postponing For Later

---
+++ backend/notification-service/service_steps.sh
@@ -0,0 +1,9 @@
+npm init -y
+npm i express zod nanoid mysql2
+npm i -D typescript tsx @types/node @types/express
+tsc --init
+mkdir src
+cd src
+mkdir config controllers routers services models types schemas
+touch app.ts server.ts
+
---
+++ backend/notification-service/src/app.ts
@@ -0,0 +1,6 @@
+import notificationRouter from "../src/routers/router.notification.js";
+import express from "express";
+
+const app = express();
+
+export default app;
